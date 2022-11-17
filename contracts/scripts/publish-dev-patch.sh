fea3365381b782d76aa03ca329d4e6f516cd093b
jaybuidl <jb@kleros.io>
2022-11-17 23:26:36 +0000

    chore: publish to npm

---
+++ contracts/scripts/publish-dev-patch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+#--------------------------------------
+# Error handling
+#--------------------------------------
+
+set -Ee
+function _catch {
+    # Don't propagate to outer shell
+    exit 0 
+}
+function _finally {
+    # TODO: rollback version bump
+    rm -rf $SCRIPT_DIR/../dist
+}
+trap _catch ERR
+trap _finally EXIT
+
+#--------------------------------------
+
+yarn version patch
+
+mkdir dist
+cp -pr README.md deployments src/ dist/ 
+rm -rf dist/test
+jq 'del(.scripts.prepare)' package.json > dist/package.json
+
+cd dist
+npm publish $@
+cd -
