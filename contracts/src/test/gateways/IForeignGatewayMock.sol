afc5d462b845d41237a47edc4f457645f6dc244b
adi274 <adityasharma.srt@gmail.com>
2022-11-23 18:28:56 +0530

    fix: gateway ocks

--- contracts/src/test/gateways/IForeignGatewayMock.sol
+++ contracts/src/test/gateways/IForeignGatewayMock.sol
@@ -9,18 +9,28 @@
  */
 
 pragma solidity ^0.8.0;
+import "../../interfaces/IReceiverGateway.sol";
 
-import "./IArbitrator.sol";
-import "./IReceiverGatewayMock.sol";
-
-interface IForeignGatewayMock is IArbitrator, IReceiverGatewayMock {
+interface IForeignGatewayMock is IReceiverGateway {
     /**
      * Relay the rule call from the home gateway to the arbitrable.
      */
+
+    function createDispute(uint256 _choices, bytes calldata _extraData) external payable returns (uint256 disputeID);
+
+    /**
+     * @dev Compute the cost of arbitration. It is recommended not to increase it often, as it can be highly time and gas consuming for the arbitrated contracts to cope with fee augmentation.
+     * @param _extraData Can be used to give additional info on the dispute to be created.
+     * @return cost Required cost of arbitration.
+     */
+    function arbitrationCost(bytes calldata _extraData) external view returns (uint256 cost);
+
     function relayRule(address _messageSender, bytes32 _disputeHash, uint256 _ruling, address _forwarder) external;
 
     function withdrawFees(bytes32 _disputeHash) external;
 
     // For cross-chain Evidence standard
     function disputeHashToForeignID(bytes32 _disputeHash) external view returns (uint256);
+
+    function receiveMessage(address _messageSender) external;
 }
