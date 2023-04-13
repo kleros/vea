992a63dd5407d87de9d39f9ffaf8b8bf5b49fd6f
marino <102478601+kemuru@users.noreply.github.com>
2023-04-13 10:21:16 +0200

    feat: mappings finished

--- veascan-subgraph-inbox/src/VeaInbox.ts
+++ veascan-subgraph-inbox/src/VeaInbox.ts
@@ -3,8 +3,9 @@ import { Snapshot, Message, Refs } from "../generated/schema";
 import {
   MessageSent,
   SnapshotSaved,
-  StaterootSent,
-} from "../generated/VeaInbox/VeaInbox";
+  SnapshotSent,
+  VeaInboxArbToGnosis,
+} from "../generated/VeaInboxArbToGnosis/VeaInboxArbToGnosis";
 
 export function handleMessageSent(event: MessageSent): void {
   const snapshot = getCurrentSnapshot();
@@ -56,21 +57,47 @@ function useNextMessageIndex(): BigInt {
 }
 
 export function handleSnapshotSaved(event: SnapshotSaved): void {
-  const snapshot = getCurrentSnapshot();
-  snapshot.taken = true;
-  snapshot.epoch = event.params.epoch;
-  snapshot.stateRoot = event.params.stateRoot;
-  snapshot.timestamp = event.block.timestamp;
-  snapshot.txHash = event.transaction.hash;
-  snapshot.save();
+  const currentSnapshot = getCurrentSnapshot();
+  currentSnapshot.taken = true;
+  currentSnapshot.stateRoot = event.params.stateRoot;
+  currentSnapshot.timestamp = event.block.timestamp;
+  currentSnapshot.txHash = event.transaction.hash;
+  //Get the epochPeriod from the public variable of the deployed contract
+  const veaInboxContract = VeaInboxArbToGnosis.bind(event.address);
+  const epochPeriod = veaInboxContract.epochPeriod();
+  const epoch = event.block.timestamp.div(epochPeriod);
+  currentSnapshot.epoch = epoch;
+  currentSnapshot.save();
+
+  // Create a new snapshot entity to be the current snapshot.
+  const refs = Refs.load("0")!;
+  const newSnapshot = new Snapshot(
+    refs.currentSnapshotIndex.plus(BigInt.fromI32(1)).toString()
+  );
+  newSnapshot.numberMessages = BigInt.fromI32(0);
+  newSnapshot.taken = false;
+  newSnapshot.resolving = false;
+  newSnapshot.save();
+
+  // Update the value of currentSnapshotIndex to point to the new snapshot.
+  refs.currentSnapshotIndex = refs.currentSnapshotIndex.plus(BigInt.fromI32(1));
+  refs.save();
 }
 
-export function handleStaterootSent(event: StaterootSent): void {
-  const snapshot = getCurrentSnapshot();
-  snapshot.resolving = true;
-  snapshot.epoch = event.params.epoch;
-  snapshot.stateRoot = event.params.stateRoot;
-  snapshot.timestamp = event.block.timestamp;
-  snapshot.txHash = event.transaction.hash;
-  snapshot.save();
+export function handleSnapshotSent(event: SnapshotSent): void {
+  const epochSent = event.params.epochSent;
+  let snapshot: Snapshot | null;
+  const refs = Refs.load("0")!;
+
+  for (let i = refs.currentSnapshotIndex.toI32(); i >= 0; i--) {
+    const snapshotId = BigInt.fromI32(i).toString();
+    snapshot = Snapshot.load(snapshotId);
+
+    if (snapshot && snapshot.epoch === epochSent) {
+      // Snapshot found, update resolving field and save
+      snapshot.resolving = true;
+      snapshot.save();
+      break;
+    }
+  }
 }
