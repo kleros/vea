3221146403f993a6a2d81b4fff7ba666bce36cf1
nhestrompia <nhestrompia@gmail.com>
2023-04-17 13:10:31 +0300

    feat: add color tag

--- veascan-web/src/styles/themes.ts
+++ veascan-web/src/styles/themes.ts
@@ -14,5 +14,13 @@ export const theme = {
     lightBlue: "#BECCE5",
     secondaryBlue: "#42498F",
     secondaryPurple: "#200F47",
+    lightRed: "#FF5A78",
+    lightYellow: "#FFC46B",
+    turquoise: "#6CC5FF",
+    lightPurple: "#B45FFF",
+    darkBlue: "#7E93FF",
+    smoke: "#BECCE5",
+    teal: "#00B8C4",
+    green: "#65DC7F",
   },
 };
