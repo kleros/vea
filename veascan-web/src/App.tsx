0200f8cb81f8b3d8a9459a9e3efca7869d5aaa52
nhestrompia <nhestrompia@gmail.com>
2023-04-18 13:18:39 +0300

    fix: dot color adjustments

--- veascan-web/src/App.tsx
+++ veascan-web/src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import { FilterDropdown } from "./components/FilterDropdown";
 
 const App = () => (
   <div>
     <Navbar />
     <Footer />
-    <FilterDropdown />
   </div>
 );
 
--- veascan-web/src/components/FilterDropdown.tsx
+++ veascan-web/src/components/FilterDropdown.tsx
@@ -1,17 +1,18 @@
 import { DropdownSelect } from "@kleros/ui-components-library";
 import React from "react";
 import styled from "styled-components";
+import { theme } from "~src/styles/themes";
 
 const ITEMS = [
   { text: "All", dot: "white", value: 1 },
-  { text: "Invalid", dot: "lightRed", value: 2 },
-  { text: "Taken", dot: "lightYellow", value: 3 },
-  { text: "Claimed", dot: "turquoise", value: 4 },
-  { text: "Challenged", dot: "lightPurple", value: 5 },
-  { text: "Verified", dot: "darkBlue", value: 6 },
-  { text: "Expired", dot: "smoke", value: 7 },
-  { text: "Resolving", dot: "teal", value: 8 },
-  { text: "Resolved", dot: "green", value: 9 },
+  { text: "Invalid", dot: theme.color.lightRed, value: 2 },
+  { text: "Taken", dot: theme.color.lightYellow, value: 3 },
+  { text: "Claimed", dot: theme.color.turquoise, value: 4 },
+  { text: "Challenged", dot: theme.color.lightPurple, value: 5 },
+  { text: "Verified", dot: theme.color.darkBlue, value: 6 },
+  { text: "Expired", dot: theme.color.smoke, value: 7 },
+  { text: "Resolving", dot: theme.color.teal, value: 8 },
+  { text: "Resolved", dot: theme.color.green, value: 9 },
 ];
 
 const FilterItem = styled(DropdownSelect)`
@@ -21,15 +22,5 @@ const FilterItem = styled(DropdownSelect)`
 `;
 
 export const FilterDropdown = () => {
-  return (
-    <div>
-      <DropdownSelect
-        items={ITEMS}
-        defaultValue={1}
-        callback={() => {
-          // function called when an item is clicked with it's value as argument
-        }}
-      />
-    </div>
-  );
+  return <FilterItem items={ITEMS} defaultValue={1} callback={() => {}} />;
 };
