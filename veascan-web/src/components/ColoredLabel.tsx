71a74e00ffd0289b3d906d607abcf1bb57f4e920
nhestrompia <nhestrompia@gmail.com>
2023-04-18 12:25:33 +0300

    chore: refactor

--- veascan-web/src/components/ColoredLabel.tsx
+++ veascan-web/src/components/ColoredLabel.tsx
@@ -3,13 +3,14 @@ import React from "react";
 import styled from "styled-components";
 
 interface ColoredLabelProps {
-  variant: string;
+  variant: keyof typeof variantColors;
   text: string;
 }
 
 const ColorTag = styled(Tag)<{ backgroundColor: string }>`
   background-color: ${({ theme, backgroundColor }) =>
     theme.color[backgroundColor]};
+  pointer-events: none;
   p {
     color: ${({ theme }) => theme.color.secondaryPurple};
     font-size: 14px;
@@ -28,10 +29,9 @@ const variantColors = {
   Resolved: "green",
 };
 
-export const ColoredLabel: React.FC<ColoredLabelProps> = ({
-  text,
-  variant,
-}) => {
+const ColoredLabel: React.FC<ColoredLabelProps> = ({ text, variant }) => {
   const backgroundColor = variantColors[variant];
   return <ColorTag text={text} backgroundColor={backgroundColor} />;
 };
+
+export default ColoredLabel;
