52a6c58a3fc0d9eed2a86d8560fa08e29d68c5cd
marino <102478601+kemuru@users.noreply.github.com>
2023-04-20 18:48:14 +0200

    feat: modularize getFormattedDataForAccordion()

--- veascan-web/src/App.tsx
+++ veascan-web/src/App.tsx
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import SnapshotAccordion from "./components/SnapshotAccordion/SnapshotAccordion";
+import { getChain } from "./consts/bridges";
 
 const MiddleContent = styled.div`
   display: flex;
@@ -13,64 +14,55 @@ const MiddleContent = styled.div`
   width: 100%;
 `;
 
+const getFormattedDataForAccordion = (data: any) => {
+  const formattedDataForAccordion = data?.[0]?.map((item) => ({
+    titleProps: {
+      epoch: item?.epoch,
+      timestamp: item?.timestamp,
+      fromChain: "Arbitrum",
+      fromAddress: "0x123456789abcdef",
+      toChain: "Ethereum",
+      toAddress: "0x987654321fedcba",
+      status: "Resolved",
+    },
+    bodyProps: {
+      snapshotDetailsProps: {
+        title: "Verifier",
+        chain: "Arbitrum",
+        txHash: item?.txHash,
+        timestamp: item?.timestamp,
+        caller: "0x1234585f4ecaab46b138ec8d87238da442eeab9b",
+        extraFields: [
+          {
+            key: "State Root",
+            value: item?.stateRoot,
+            isCopy: true,
+          },
+        ],
+      },
+    },
+  }));
+  return formattedDataForAccordion;
+};
+
 const App = () => {
   // get query data
   const { data } = useSnapshots("999999999999");
+  console.log(data);
 
-  // then generate an Array of objects with the necessary fields
-  let dataForSnapshotAccordion;
-  if (data) {
-    console.log(data);
-    dataForSnapshotAccordion = data?.[0]?.map((item) => ({
-      titleProps: {
-        epoch: item?.epoch,
-        timestamp: item?.timestamp,
-        fromChain: "Arbitrum",
-        fromAddress: "0x123456789abcdef",
-        toChain: "Ethereum",
-        toAddress: "0x987654321fedcba",
-        status: "Resolved",
-      },
-      bodyProps: {
-        snapshotDetailsProps: {
-          title: "Verifier",
-          chain: "Arbitrum",
-          txHash: item?.txHash,
-          timestamp: item?.timestamp,
-          caller: "0x1234585f4ecaab46b138ec8d87238da442eeab9b",
-          extraFields: [
-            {
-              key: "State Root",
-              value: item?.stateRoot,
-              isCopy: true,
-            },
-          ],
-        },
-      },
-    }));
-    console.log(dataForSnapshotAccordion);
-  }
-
-  if (data) {
-    return (
-      <div>
-        <Navbar />
-        <MiddleContent>
-          <SnapshotAccordion items={dataForSnapshotAccordion} />
-        </MiddleContent>
-        <Footer />
-      </div>
-    );
-  } else
-    return (
-      <div>
-        <Navbar />
-        <MiddleContent>
+  return (
+    <div>
+      <Navbar />
+      <MiddleContent>
+        {data ? (
+          <SnapshotAccordion items={getFormattedDataForAccordion(data)} />
+        ) : (
           <p>loading...</p>
-        </MiddleContent>
-        <Footer />
-      </div>
-    );
+        )}
+      </MiddleContent>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
