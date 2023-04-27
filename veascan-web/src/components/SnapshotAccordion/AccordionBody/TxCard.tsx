6028bac741f815dbdb1637c2a9fce7fb4950e1b1
nhestrompia <nhestrompia@gmail.com>
2023-04-27 23:15:19 +0300

    feat: add txcard mobile responsiveness

--- veascan-web/src/components/SnapshotAccordion/AccordionBody/TxCard.tsx
+++ veascan-web/src/components/SnapshotAccordion/AccordionBody/TxCard.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import styled from "styled-components";
-import Copy from "tsx:svgs/icons/copy.svg";
 import { getChain } from "consts/bridges";
+import React from "react";
+import styled, { css } from "styled-components";
+import Copy from "tsx:svgs/icons/copy.svg";
+import { smallScreenStyle } from "~src/styles/smallScreenStyle";
 
 interface Field {
   key: string;
@@ -21,33 +22,61 @@ export interface TxCardProps {
 
 const StyledDiv = styled.div`
   width: auto;
-  .icon {
-    width: 16px;
-    height: 16px;
-    fill: none;
-    cursor: pointer;
-  }
+
+  ${smallScreenStyle(css`
+    width: 307px;
+    word-wrap: break-word;
+    margin-left: 2px;
+  `)}
+
   .tx-info-copyable {
     color: ${({ theme }) => theme.klerosUIComponentsPrimaryBlue} !important;
     text-decoration: none;
   }
   .tx-info {
-    display: grid;
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+    ${smallScreenStyle(css`
+      gap: 16px;
+    `)}
     width: fit-content;
+  }
+  .tx-mobile-chain {
+    display: grid;
     grid-row: 2;
     gap: 108px;
+    width: fit-content;
+
+    ${smallScreenStyle(css`
+      display: flex;
+      flex-direction: row;
+      gap: 16px;
+    `)};
+  }
+  .tx-info-layout {
+    display: grid;
+    grid-row: 2;
+    gap: 108px;
+    ${smallScreenStyle(css`
+      display: flex;
+      flex-direction: column;
+      gap: 4px;
+    `)}
+    width: fit-content;
   }
   .tx-info-titles {
     grid-column-start: 1;
     display: flex;
     flex-direction: column;
     gap: 4px;
-    width: fit-content;
+    width: 106px;
   }
   .tx-info-data {
     grid-column-start: 2;
     display: flex;
     flex-direction: column;
+    width: auto;
     gap: 4px;
   }
   .border {
@@ -61,8 +90,29 @@ const ValueDiv = styled.div`
   display: flex;
   align-items: center;
   gap: 9px;
+
+  ${smallScreenStyle(css`
+    width: 100%;
+    gap: 4px;
+    word-break: break-all;
+    &.chain-info {
+      flex-direction: row;
+      gap: 16px;
+    }
+  `)}
+
+  .tx-copy-div {
+    display: flex;
+    gap: 9px;
+    ${smallScreenStyle(css`
+      display: block;
+    `)}
+  }
   &.chain-info {
     gap: 4px;
+  }
+  &.copyable {
+    flex-direction: row-reverse;
   }
 `;
 
@@ -70,6 +120,7 @@ const Icon = styled.svg`
   width: 16px;
   height: 16px;
   fill: none;
+  ${smallScreenStyle(css``)}
 `;
 
 const Header = styled.label`
@@ -116,52 +167,65 @@ const TxCard: React.FC<TxCardProps> = ({
     <StyledDiv>
       <Header>{title}</Header>
       <div className="tx-info">
-        <div className="tx-info-titles">
-          {fields.map((section, index) => (
-            <small key={index}>{section.key}</small>
-          ))}
-        </div>
-        <div className="tx-info-data">
-          {fields.map((section, index) => {
-            if (section.key === "Chain") {
-              return (
-                <ValueDiv className="chain-info" key={index}>
+        {fields.map((section, index) => (
+          <div
+            className={` ${
+              section.key === "Chain" ? "tx-mobile-chain" : "tx-info-layout"
+            }`}
+            key={index}
+          >
+            <div className="tx-info-titles">
+              <small>{section.key}</small>
+            </div>
+            <div className="tx-info-data">
+              {section.key === "Chain" ? (
+                <ValueDiv className="chain-info">
                   <Icon as={chainObject?.logo} />
                   <small>{section.value}</small>
                 </ValueDiv>
-              );
-            } else if (!section.isCopy) {
-              return <small key={index}>{section.value}</small>;
-            } else {
-              return (
-                <ValueDiv key={index}>
+              ) : (
+                <ValueDiv>
                   {section.url ? (
-                    <a
-                      href={section.url}
-                      target="_blank"
-                      style={{ textDecoration: "none" }}
-                      rel="noreferrer"
-                    >
-                      <small className="tx-info-copyable">
-                        {section.value}
-                      </small>
-                    </a>
+                    <div className="tx-copy-div">
+                      <a
+                        href={section.url}
+                        target="_blank"
+                        style={{ textDecoration: "none" }}
+                        rel="noreferrer"
+                      >
+                        <small className="tx-info-copyable">
+                          {section.value}
+                        </small>
+                      </a>{" "}
+                      {section.isCopy && (
+                        <button
+                          onClick={() => {
+                            navigator.clipboard.writeText(section.value!);
+                          }}
+                        >
+                          <Icon as={Copy} />
+                        </button>
+                      )}
+                    </div>
                   ) : (
-                    <small>{section.value}</small>
+                    <div className="tx-copy-div">
+                      <small>{section.value} </small>
+                      {section.isCopy && (
+                        <button
+                          onClick={() => {
+                            navigator.clipboard.writeText(section.value!);
+                          }}
+                        >
+                          <Icon as={Copy} />
+                        </button>
+                      )}
+                    </div>
                   )}
-                  <button>
-                    <Icon
-                      as={Copy}
-                      onClick={() => {
-                        navigator.clipboard.writeText(section.value!);
-                      }}
-                    />
-                  </button>
                 </ValueDiv>
-              );
-            }
-          })}
-        </div>
+              )}
+            </div>
+          </div>
+        ))}
       </div>
       <hr className="border" />
     </StyledDiv>
