318d1be5cc5bb35f6fc3b16733d8bdea44417be1
alcercu <333aleix333@gmail.com>
2023-04-28 12:38:42 +0200

    fix(web): make filter headers responsive

--- veascan-web/src/components/TxFilterHeader.tsx
+++ veascan-web/src/components/TxFilterHeader.tsx
@@ -2,11 +2,9 @@ import { IChain, supportedChains } from "consts/bridges";
 import React, { FC } from "react";
 import styled, { css } from "styled-components";
 import Globe from "tsx:svgs/icons/globe.svg";
-import { smallScreenStyle } from "~src/styles/smallScreenStyle";
-import { theme } from "~src/styles/themes";
+import { smallScreenStyle } from "styles/smallScreenStyle";
+import { theme } from "styles/themes";
 import { FilterDropdown } from "./FilterDropdown";
-
-interface TxFilterHeaderProps {}
 
 const STATUS_ITEMS = [
   { text: "All", dot: theme.color.white, value: 1 },
@@ -40,37 +38,24 @@ const SnapshotHeader = styled.div`
   flex-direction: column;
   gap: 64px;
   margin: 76px auto 24px auto;
-  width: 1170px;
-
-  ${smallScreenStyle(css`
-    margin-top: 48px;
-    margin-bottom: 0px;
-    width: 342px;
-  `)}
+  width: 100%;
 `;
 
 const FilterHeader = styled.div`
   display: flex;
   margin-left: 33px;
   align-items: start;
-
+  flex-wrap: wrap;
+  justify-content: space-between;
   ${smallScreenStyle(css`
-    flex-direction: column;
-    margin-left: 0px;
-    height: 108px;
+    margin-left: calc(0px + (33) * (100vw - 370px) / (1250 - 370));
+    gap: 12px;
   `)}
 `;
 
-const NetworkTag = styled.div`
+const NetworkFilters = styled.div`
   display: flex;
   gap: 50px;
-  margin-right: 309.5px;
-
-  ${smallScreenStyle(css`
-    flex-direction: row;
-    gap: 24px;
-    margin-right: 0px;
-  `)}
 `;
 
 const DropdownTag = styled.div`
@@ -84,22 +69,6 @@ const DropdownTag = styled.div`
     line-height: 17.5px;
     color: ${({ theme }) => theme.color.blue} !important;
   }
-
-  ${smallScreenStyle(css`
-    align-items: normal;
-  `)}
-`;
-
-const StyledStatusDropdownTag = styled(DropdownTag)`
-  ${smallScreenStyle(css`
-    margin-top: 24px;
-  `)}
-`;
-
-const StyledToNetworkDropdownTag = styled(DropdownTag)`
-  ${smallScreenStyle(css`
-    gap: 4px;
-  `)}
 `;
 
 const EpochAndTimeTag = styled.div`
@@ -120,7 +89,7 @@ const EpochAndTimeTag = styled.div`
   `)}
 `;
 
-const TxFilterHeader: FC<TxFilterHeaderProps> = ({}) => {
+const TxFilterHeader: FC = () => {
   return (
     <SnapshotHeader>
       <HeaderText>Latest Snapshots</HeaderText>
@@ -129,7 +98,7 @@ const TxFilterHeader: FC<TxFilterHeaderProps> = ({}) => {
           <small>Epoch ID</small>
           <small>Timestamp</small>
         </EpochAndTimeTag>
-        <NetworkTag>
+        <NetworkFilters>
           <DropdownTag>
             <small>From: </small>
             <FilterDropdown
@@ -139,23 +108,23 @@ const TxFilterHeader: FC<TxFilterHeaderProps> = ({}) => {
             />
           </DropdownTag>
 
-          <StyledToNetworkDropdownTag>
+          <DropdownTag>
             <small>To: </small>
             <FilterDropdown
               isAlignRight={false}
               isSimpleButton
               itemData={CHAIN_ITEMS}
             />
-          </StyledToNetworkDropdownTag>
-        </NetworkTag>
-        <StyledStatusDropdownTag>
+          </DropdownTag>
+        </NetworkFilters>
+        <DropdownTag>
           <small>Status: </small>
           <FilterDropdown
             isAlignRight={true}
             isSimpleButton
             itemData={STATUS_ITEMS}
           />
-        </StyledStatusDropdownTag>
+        </DropdownTag>
       </FilterHeader>
     </SnapshotHeader>
   );
