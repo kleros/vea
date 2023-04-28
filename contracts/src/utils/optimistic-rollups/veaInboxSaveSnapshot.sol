04a019eff82d393bdfc4636069bddff859041ecd
shotaronowhere <shawtarohgn@gmail.com>
2023-04-28 12:31:13 +0800

    feat(contracts): inbox calldata optimization

---
+++ contracts/src/utils/optimistic-rollups/veaInboxSaveSnapshot.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+/**
+ *  @authors: [@shotaronowhere]
+ *  @reviewers: []
+ *  @auditors: []
+ *  @bounties: []
+ *  @deployments: []
+ */
+
+pragma solidity 0.8.18;
+
+import "../../interfaces/IVeaInbox.sol";
+
+/**
+ * Vea Inbox veaInboxSaveSnapshot Calldata Optimization.
+ * No function selector required, only fallback function.
+ */
+contract veaInboxSaveSnapshot {
+    IVeaInbox public immutable veaInbox;
+
+    constructor(IVeaInbox _veaInbox) {
+        veaInbox = _veaInbox;
+    }
+
+    fallback() external {
+        veaInbox.saveSnapshot();
+    }
+}
