e8264d85cb95e06861e2d121beaf71993ff11ee6
nhestrompia <nhestrompia@gmail.com>
2023-05-02 22:11:21 +0300

    chore: refactor

--- veascan-web/src/components/SnapshotAccordion/AccordionTitle/ChainAndAddress.tsx
+++ veascan-web/src/components/SnapshotAccordion/AccordionTitle/ChainAndAddress.tsx
@@ -1,36 +1,35 @@
 import React from "react";
 import styled from "styled-components";
 
+const StyledChainAndAddress = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+`;
+
+const StyledTruncatedAddress = styled.a`
+  display: flex;
+  padding-top: 3.5px;
+  color: ${({ theme }) => theme.color.blue};
+  text-decoration: none;
+
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+const ChainIcon = styled.svg`
+  position: relative;
+  width: 24px;
+  height: 28px;
+  fill: none;
+  margin-right: 8px;
+  padding-bottom: 2px;
+`;
 const ChainAndAddress: React.FC<{
   chainObject: any;
   address: string;
 }> = (p) => {
-  const StyledChainAndAddress = styled.div`
-    position: relative;
-    display: flex;
-    flex-direction: row;
-  `;
-
-  const StyledTruncatedAddress = styled.a`
-    display: flex;
-    padding-top: 3.5px;
-    color: ${({ theme }) => theme.color.blue};
-    text-decoration: none;
-
-    :hover {
-      text-decoration: underline;
-    }
-  `;
-
-  const ChainIcon = styled.svg`
-    position: relative;
-    width: 24px;
-    height: 28px;
-    fill: none;
-    margin-right: 8px;
-    padding-bottom: 2px;
-  `;
-
   const truncatedAddress = `${p.address.slice(0, 6)}...${p.address.slice(-4)}`;
 
   return (
--- veascan-web/src/components/SnapshotAccordion/AccordionTitle/Timestamp.tsx
+++ veascan-web/src/components/SnapshotAccordion/AccordionTitle/Timestamp.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 
+const StyledTimestamp = styled.div`
+  color: ${({ theme }) => theme.color.lightBlue};
+  white-space: nowrap;
+`;
 const Timestamp: React.FC<{
   timestamp: string;
 }> = (p) => {
-  const StyledTimestamp = styled.div`
-    color: ${({ theme }) => theme.color.lightBlue};
-    white-space: nowrap;
-  `;
-
   return <StyledTimestamp>{p.timestamp} </StyledTimestamp>;
 };
 
