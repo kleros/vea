9e1f271a9a3365574fcf2bf7f56db15d621e2d76
jaybuidl <jb@kleros.io>
2023-05-30 00:28:36 +0100

    feat: improved release process, fixed a circular-dep, re-released

--- vea-sdk/src/utils/envVar.ts
+++ vea-sdk/src/utils/envVar.ts
@@ -1,14 +1,11 @@
 import dotenv from "dotenv";
-import logger from "./logger";
 
 dotenv.config();
 
 function envVar(key: string): string {
   const value = process.env[key];
   if (value === undefined) {
-    const error = new Error(`Environment variable ${key} is undefined`);
-    logger.error(error);
-    throw error;
+    throw new Error(`Environment variable ${key} is undefined`);
   }
   return value;
 }
