c5abc81cf656a06dc3ed06a8bbcd82230a9321c6
shotaronowhere <shawtarohgn@gmail.com>
2023-10-12 17:38:48 -0700

    feat: watcher utility functions

---
+++ contracts/src/utils/L1/veaOutboxMultiChallenge.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+/// @custom:authors: [@shotaronowhere]
+/// @custom:reviewers: []
+/// @custom:auditors: []
+/// @custom:bounties: []
+/// @custom:deployments: []
+
+pragma solidity 0.8.18;
+
+/// @dev Vea Inbox Calldata Optimization.
+///      No function selector required, only fallback function.
+contract veaOutboxMultiChallenge {
+    address public immutable veaOutboxOnL1;
+    uint256 public immutable deposit;
+
+    constructor(address _veaOutboxOnL1, uint256 _deposit) {
+        veaOutboxOnL1 = _veaOutboxOnL1;
+        deposit = _deposit;
+    }
+
+    function multiChallenge(bytes[] calldata datas) external payable {
+        for (uint256 i = 0; i < datas.length; i++) {
+            veaOutboxOnL1.call{value: deposit}(datas[i]);
+        }
+
+        uint256 balance = address(this).balance;
+        if (balance > 0) payable(msg.sender).send(balance - 1); // msg.sender responsible to accept eth
+    }
+}
---
+++ contracts/src/utils/L1/veaOutboxMultiChallengeWETH.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+/// @custom:authors: [@shotaronowhere]
+/// @custom:reviewers: []
+/// @custom:auditors: []
+/// @custom:bounties: []
+/// @custom:deployments: []
+
+pragma solidity 0.8.18;
+
+import "../../interfaces/tokens/gnosis/IWETH.sol";
+
+/// @dev Vea Inbox Calldata Optimization.
+///      No function selector required, only fallback function.
+contract veaOutboxMultiChallengeWETH {
+    address public immutable veaOutboxOnL1;
+    IWETH public immutable weth;
+    uint256 public immutable deposit;
+
+    constructor(address _veaOutboxOnL1, address _weth, uint256 _deposit) {
+        veaOutboxOnL1 = _veaOutboxOnL1;
+        weth = IWETH(_weth);
+        require(weth.approve(_veaOutboxOnL1, type(uint256).max), "Failed WETH approve.");
+        deposit = _deposit;
+    }
+
+    function multiChallenge(bytes[] calldata datas) external payable {
+        require(weth.transferFrom(msg.sender, address(this), deposit), "Failed WETH transfer.");
+        for (uint256 i; i < datas.length; i++) {
+            veaOutboxOnL1.call(datas[i]);
+        }
+        uint256 balance = weth.balanceOf(address(this));
+        if (balance > 0) weth.transfer(msg.sender, balance - 1);
+    }
+}
