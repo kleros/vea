a4b6a9045272df7f0de667337f325b6fe398236b
Mani Brar <manibrar@Manis-MacBook-Pro.local>
2024-10-21 12:20:53 +0530

    feat(relayer): sep & chiado testnet

---
+++ relayer-cli/src/testnet/arbSepToSepRelayer.ts
@@ -0,0 +1,38 @@
+import * as fs from "fs";
+require("dotenv").config();
+import { relayBatch } from "utils/relay";
+import { initialize, updateStateFile } from "utils/relayerHelpers";
+
+const _contract = require("@kleros/vea-contracts/deployments/sepolia/VeaOutboxArbToEthTestnet.json");
+const network = "testnet";
+
+let chain_id = 11155111;
+const epochPeriod = 7200; // 3 hrs
+const batchSize = 10; // 10 messages per batch
+
+["SIGINT", "SIGTERM", "SIGQUIT", "EXIT", "MODULE_NOT_FOUND"].forEach((signal) =>
+  process.on(signal, async () => {
+    console.log("exit");
+    const lock_file_name = "./src/state/" + chain_id + ".pid";
+    if (fs.existsSync(lock_file_name)) {
+      fs.unlinkSync(lock_file_name);
+    }
+    process.exit(0);
+  })
+);
+
+(async () => {
+  while (1) {
+    let nonce = await initialize(chain_id, network);
+    nonce = await relayBatch(chain_id, nonce, batchSize, _contract);
+    if (nonce != null) await updateStateFile(chain_id, Math.floor(Date.now() / 1000), nonce, network);
+    const currentTS = Math.floor(Date.now() / 1000);
+    const delayAmount = (epochPeriod - (currentTS % epochPeriod)) * 1000 + 100 * 1000;
+    console.log("waiting for the next epoch. . .", Math.floor(delayAmount / 1000), "seconds");
+    await delay(delayAmount);
+  }
+})();
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
