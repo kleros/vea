64427bbbaa7d4688bd2b314c2ce884013532f63c
Mani Brar <manibrar@Manis-MacBook-Pro.local>
2024-12-11 16:43:34 +0700

    chore: refactored docker config

--- contracts/start-forks.sh
+++ contracts/start-forks.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-if [ -z "$INFURA_ID" ]; then
-  echo "Error: INFURA_ID is not set."
+if [ -z "$INFURA_API_KEY" ]; then
+  echo "Error: INFURA_API_KEY is not set."
+  exit 1
+fi
+
+if [ -z "$NETWORK" ]; then
+  echo "Error: NETWORK is not set for $PORT."
   exit 1
 fi
 
 # Set for 8545: Arbitrum Sepolia fork
-echo "Starting Hardhat fork for $NETWORK1 on port $PORT1..."
-npx hardhat node --fork https://$NETWORK1.infura.io/v3/$INFURA_ID --no-deploy --hostname 0.0.0.0 --port $PORT1 &
-
-# Set for 8546: Sepolia fork
-echo "Starting Hardhat fork for $NETWORK2 on port $PORT2..."
-npx hardhat node --fork https://$NETWORK2.infura.io/v3/$INFURA_ID --no-deploy --hostname 0.0.0.0 --port $PORT2 &
+echo "Starting Hardhat fork for $NETWORK on port $PORT..."
+npx hardhat node --fork https://$NETWORK.infura.io/v3/$INFURA_API_KEY --no-deploy --hostname 0.0.0.0 --port $PORT &
 
 wait
