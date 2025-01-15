9a19159c0d230668799aeadfa507d9532add7ebf
Mani Brar <manibrar@Manis-MacBook-Pro.local>
2025-01-15 12:27:22 +0530

    feat(devnet): logger

--- relayer-cli/src/devnetRelayExample.ts
+++ relayer-cli/src/devnetRelayExample.ts
@@ -1,26 +1,29 @@
+import { EventEmitter } from "events";
 import { relayAllFrom } from "./utils/relay";
 import { initialize, ShutdownManager, updateStateFile, setupExitHandlers, delay } from "./utils/relayerHelpers";
+import { BotEvents } from "utils/botEvents";
 
-export async function start(shutdownManager: ShutdownManager = new ShutdownManager()) {
+export async function start(shutdownManager: ShutdownManager = new ShutdownManager(), emitter: EventEmitter) {
   const chainId = parseInt(process.env.VEAOUTBOX_CHAIN_ID);
   const epochPeriod = 1800; // 30 min
   const network = "devnet";
-  await setupExitHandlers(chainId, shutdownManager, network);
+  await setupExitHandlers(chainId, shutdownManager, network, emitter);
   while (!shutdownManager.getIsShuttingDown()) {
-    let nonce = await initialize(chainId, network);
+    let nonce = await initialize(chainId, network, emitter);
     // This is libghtbulb switch address in arbitrum sepolia
     const sender = process.env.DEVNET_SENDER;
     nonce = await relayAllFrom(chainId, nonce, sender);
-    if (nonce != null) await updateStateFile(chainId, Math.floor(Date.now() / 1000), nonce, network);
+    if (nonce != null) await updateStateFile(chainId, Math.floor(Date.now() / 1000), nonce, network, emitter);
 
     const currentTS = Math.floor(Date.now() / 1000);
     const delayAmount = (epochPeriod - (currentTS % epochPeriod)) * 1000 + 100 * 1000;
-    console.log("waiting for the next epoch. . .", Math.floor(delayAmount / 1000), "seconds");
+    emitter.emit(BotEvents.WAITING, delayAmount);
     await delay(delayAmount);
   }
 }
 
 if (require.main === module) {
+  const emitter = new EventEmitter();
   const shutdownManager = new ShutdownManager(false);
-  start(shutdownManager);
+  start(shutdownManager, emitter);
 }
