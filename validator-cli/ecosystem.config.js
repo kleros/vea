c3d2a2347e48be3234308420ecc3a3bc056b0af3
Mani Brar <manibrar@Manis-MacBook-Pro.local>
2025-04-07 10:41:24 +0530

    chore(validator): update pm2 config

--- validator-cli/ecosystem.config.js
+++ validator-cli/ecosystem.config.js
@@ -6,8 +6,8 @@ module.exports = {
       interpreter: "../node_modules/.bin/ts-node",
       interpreter_args: "--project tsconfig.json -r tsconfig-paths/register",
       log_date_format: "YYYY-MM-DD HH:mm Z",
-      watch: false,
-      autorestart: false,
+      watch: true,
+      autorestart: true,
       env: {
         NODE_ENV: "development",
         TS_NODE_PROJECT: "./tsconfig.json",
