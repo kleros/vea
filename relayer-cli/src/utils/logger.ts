438bc01b57125b78629a4dfc25a6fe0e67f3ccda
Mani Brar <manibrar@Mac.lan>
2025-12-05 12:01:31 +0530

    feat(relayer): pino

--- relayer-cli/src/utils/logger.ts
+++ relayer-cli/src/utils/logger.ts
@@ -1,5 +1,30 @@
 import { EventEmitter } from "node:events";
+import pino from "pino";
 import { BotEvents } from "./botEvents";
+
+const logtailToken = process.env.LOGTAIL_TOKEN;
+
+const loggerOptions = {
+  level: "debug",
+  base: { service: "Vea" },
+  transport: logtailToken
+    ? {
+        target: "@logtail/pino",
+        options: {
+          sourceToken: logtailToken,
+        },
+      }
+    : {
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          translateTime: "SYS:standard",
+          ignore: "pid,hostname",
+        },
+      },
+};
+const baseLogger = pino(loggerOptions);
+const getLogger = (context: string) => baseLogger.child({ context });
 
 /**
  * Listens to relevant events of an EventEmitter instance and issues log lines
@@ -17,57 +42,58 @@ export const initialize = (emitter: EventEmitter) => {
 };
 
 export const configurableInitialize = (emitter: EventEmitter) => {
+  const logger = getLogger("Relayer");
   // Relayer state logs
   emitter.on(BotEvents.STARTED, (chainId, network) => {
-    console.log(`Relayer started for ${chainId} on ${network}`);
+    logger.info(`Relayer started for ${chainId} on ${network}`);
   });
   emitter.on(BotEvents.WAITING, (delayAmount) => {
-    console.log(`Waiting for next epoch: ${delayAmount} ms`);
+    logger.info(`Waiting for next epoch: ${delayAmount} ms`);
   });
   emitter.on(BotEvents.EXIT, () => {
-    console.log("Exiting");
+    logger.info("Exiting");
   });
 
   // Bot health logs
   emitter.on(BotEvents.EXCEPTION, (err) => {
-    console.error("Uncaught Exception occurred", err);
+    logger.error({ err }, "Uncaught Exception occurred");
   });
   emitter.on(BotEvents.PROMISE_REJECTION, (reason, promise) => {
-    console.error("Unhandled promise rejection:", reason, "at", promise);
+    logger.error({ reason, promise }, "Unhandled promise rejection");
   });
 
   // Lock file logs
   emitter.on(BotEvents.LOCK_CLAIMED, () => {
-    console.log("Lock claimed");
+    logger.debug("lock_claimed");
   });
   emitter.on(BotEvents.LOCK_DIRECTORY, (pwd) => {
-    console.log(`Lock file directory: ${pwd}`);
+    logger.debug({ pwd }, "lock_file_directory");
   });
   emitter.on(BotEvents.LOCK_RELEASED, () => {
-    console.log("Lock released");
+    logger.debug("lock_released");
   });
 
   // Message relay logs
   emitter.on(BotEvents.RELAY_BATCH, (nonce, tx) => {
-    console.log(`Relaying batch till nonce ${nonce}: ${tx}`);
+    logger.info({ nonce, tx }, "relaying_batch_till_nonce");
   });
 
   emitter.on(BotEvents.RELAY_ALL_FROM, (nonce, msgSenders, tx) => {
-    console.log(`Relaying all messages from ${msgSenders} with nonce ${nonce}: ${tx}`);
+    logger.info({ nonce, msgSenders, tx }, "relaying_all_from_till_nonce");
   });
 
   emitter.on(BotEvents.MESSAGE_EXECUTION_FAILED, (nonce) => {
-    console.error(`Message execution failed for nonce ${nonce}`);
+    logger.error({ nonce }, "message_execution_failed_for_nonce");
   });
 
   // Hashi executor logs
   emitter.on(BotEvents.EXECUTING_HASHI, (startNonce, endNonce) => {
-    console.log(`Executing Hashi for nonces from ${startNonce} to ${endNonce}`);
+    logger.debug({ startNonce, endNonce }, "executing_hashi");
   });
   emitter.on(BotEvents.HASHI_EXECUTED, (endNonce) => {
-    console.log(`Successfully executed Hashi till ${endNonce}`);
+    logger.info({ endNonce }, "hashi_executed_till_nonce");
   });
   emitter.on(BotEvents.HASHI_BATCH_TXN, (txHash, batchSize) => {
-    console.log(`Hashi batch transaction ${txHash} for ${batchSize} messages`);
+    logger.debug({ txHash, batchSize }, "hashi_batch_txn");
   });
 };
