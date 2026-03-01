c064d2fa2924cb998c057c502e9ad532743400b3
Mani Brar <manibrar@Mac.lan>
2026-03-01 18:17:48 +0530

    chore: add package

--- veashi-sdk/index.ts
+++ veashi-sdk/index.ts
@@ -1,9 +1,7 @@
-// 1. Export your logic and data
 export * from "./registry";
 export * from "./getters";
 
-// 2. Export the Types (so users get autocomplete)
-export * from "./types"; // Move your Bridge and Route types here too
-export * from "../typechain-types";
+export * from "./types";
+export * from "./typechain-types";
 
-export * from "./abiMapping"; // This exports the named ABIs
+export * from "./abiMapping";
