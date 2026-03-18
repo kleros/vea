c18958e9116a5622aa6ca1d39f3bfbc01148cbe0
Mani Brar <manibrar@Manis-MacBook-Pro.local>
2026-03-18 18:34:43 +0530

    chore: update & scope out lz contracts

---
+++ veashi-contracts/src/layerZero/interfaces/ILayerZeroReceiver.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.20;
+import {Origin} from "./ILayerZeroEndpointV2.sol";
+
+interface ILayerZeroReceiver {
+    function lzReceive(
+        Origin calldata _origin,
+        bytes32 _guid,
+        bytes calldata _message,
+        address _executor,
+        bytes calldata _extraData
+    ) external payable;
+
+    function allowInitializePath(Origin calldata _origin) external view returns (bool);
+
+    function nextNonce(uint32 _eid, bytes32 _sender) external view returns (uint64);
+}
