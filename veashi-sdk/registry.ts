5615b3869f86b402a7ec5be9e803ad410a2bf602
Mani Brar <mani99brar@gmail.com>
2026-09-02 13:35:25 +0530

    chore: sync sdk

--- veashi-sdk/registry.ts
+++ veashi-sdk/registry.ts
@@ -1,29 +1,41 @@
 import type { FlatRouteFile } from "./types";
 
-import route_0 from "./addresses/1-8453.json";
-import route_1 from "./addresses/11155111-84532.json";
-import route_2 from "./addresses/1514-42161.json";
-import route_3 from "./addresses/42161-1514.json";
-import route_4 from "./addresses/42161-8453.json";
-import route_5 from "./addresses/421614-10200.json";
-import route_6 from "./addresses/421614-11155111.json";
-import route_7 from "./addresses/421614-84532.json";
-import route_8 from "./addresses/8453-1.json";
-import route_9 from "./addresses/8453-42161.json";
-import route_10 from "./addresses/84532-11155111.json";
-import route_11 from "./addresses/84532-421614.json";
+import route_0 from "./addresses/1-4217.json";
+import route_1 from "./addresses/1-4663.json";
+import route_2 from "./addresses/1-8453.json";
+import route_3 from "./addresses/11155111-84532.json";
+import route_4 from "./addresses/1514-42161.json";
+import route_5 from "./addresses/42161-1514.json";
+import route_6 from "./addresses/42161-4663.json";
+import route_7 from "./addresses/42161-8453.json";
+import route_8 from "./addresses/421614-10200.json";
+import route_9 from "./addresses/421614-11155111.json";
+import route_10 from "./addresses/421614-84532.json";
+import route_11 from "./addresses/4217-1.json";
+import route_12 from "./addresses/4663-1.json";
+import route_13 from "./addresses/4663-42161.json";
+import route_14 from "./addresses/8453-1.json";
+import route_15 from "./addresses/8453-42161.json";
+import route_16 from "./addresses/84532-11155111.json";
+import route_17 from "./addresses/84532-421614.json";
 
 export const ROUTES: Record<string, FlatRouteFile> = {
-  "1-8453": route_0,
-  "11155111-84532": route_1,
-  "1514-42161": route_2,
-  "42161-1514": route_3,
-  "42161-8453": route_4,
-  "421614-10200": route_5,
-  "421614-11155111": route_6,
-  "421614-84532": route_7,
-  "8453-1": route_8,
-  "8453-42161": route_9,
-  "84532-11155111": route_10,
-  "84532-421614": route_11,
+  "1-4217": route_0,
+  "1-4663": route_1,
+  "1-8453": route_2,
+  "11155111-84532": route_3,
+  "1514-42161": route_4,
+  "42161-1514": route_5,
+  "42161-4663": route_6,
+  "42161-8453": route_7,
+  "421614-10200": route_8,
+  "421614-11155111": route_9,
+  "421614-84532": route_10,
+  "4217-1": route_11,
+  "4663-1": route_12,
+  "4663-42161": route_13,
+  "8453-1": route_14,
+  "8453-42161": route_15,
+  "84532-11155111": route_16,
+  "84532-421614": route_17,
 };
