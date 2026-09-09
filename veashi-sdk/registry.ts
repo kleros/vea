008804715058e7023ca243c72e6a2bb70ffa7b42
Mani Brar <mani99brar@gmail.com>
2026-09-09 13:21:02 +0530

    chore: sync sdk with new routes

--- veashi-sdk/registry.ts
+++ veashi-sdk/registry.ts
@@ -3,39 +3,47 @@ import type { FlatRouteFile } from "./types";
 import route_0 from "./addresses/1-4217.json";
 import route_1 from "./addresses/1-4663.json";
 import route_2 from "./addresses/1-8453.json";
-import route_3 from "./addresses/11155111-84532.json";
-import route_4 from "./addresses/1514-42161.json";
-import route_5 from "./addresses/42161-1514.json";
-import route_6 from "./addresses/42161-4663.json";
-import route_7 from "./addresses/42161-8453.json";
-import route_8 from "./addresses/421614-10200.json";
-import route_9 from "./addresses/421614-11155111.json";
-import route_10 from "./addresses/421614-84532.json";
-import route_11 from "./addresses/4217-1.json";
-import route_12 from "./addresses/4663-1.json";
-import route_13 from "./addresses/4663-42161.json";
-import route_14 from "./addresses/8453-1.json";
-import route_15 from "./addresses/8453-42161.json";
-import route_16 from "./addresses/84532-11155111.json";
-import route_17 from "./addresses/84532-421614.json";
+import route_3 from "./addresses/11155111-5042002.json";
+import route_4 from "./addresses/11155111-84532.json";
+import route_5 from "./addresses/1514-42161.json";
+import route_6 from "./addresses/42161-1514.json";
+import route_7 from "./addresses/42161-4217.json";
+import route_8 from "./addresses/42161-4663.json";
+import route_9 from "./addresses/42161-8453.json";
+import route_10 from "./addresses/421614-10200.json";
+import route_11 from "./addresses/421614-11155111.json";
+import route_12 from "./addresses/421614-84532.json";
+import route_13 from "./addresses/4217-1.json";
+import route_14 from "./addresses/4217-42161.json";
+import route_15 from "./addresses/4663-1.json";
+import route_16 from "./addresses/4663-42161.json";
+import route_17 from "./addresses/5042002-11155111.json";
+import route_18 from "./addresses/8453-1.json";
+import route_19 from "./addresses/8453-42161.json";
+import route_20 from "./addresses/84532-11155111.json";
+import route_21 from "./addresses/84532-421614.json";
 
 export const ROUTES: Record<string, FlatRouteFile> = {
   "1-4217": route_0,
   "1-4663": route_1,
   "1-8453": route_2,
-  "11155111-84532": route_3,
-  "1514-42161": route_4,
-  "42161-1514": route_5,
-  "42161-4663": route_6,
-  "42161-8453": route_7,
-  "421614-10200": route_8,
-  "421614-11155111": route_9,
-  "421614-84532": route_10,
-  "4217-1": route_11,
-  "4663-1": route_12,
-  "4663-42161": route_13,
-  "8453-1": route_14,
-  "8453-42161": route_15,
-  "84532-11155111": route_16,
-  "84532-421614": route_17,
+  "11155111-5042002": route_3,
+  "11155111-84532": route_4,
+  "1514-42161": route_5,
+  "42161-1514": route_6,
+  "42161-4217": route_7,
+  "42161-4663": route_8,
+  "42161-8453": route_9,
+  "421614-10200": route_10,
+  "421614-11155111": route_11,
+  "421614-84532": route_12,
+  "4217-1": route_13,
+  "4217-42161": route_14,
+  "4663-1": route_15,
+  "4663-42161": route_16,
+  "5042002-11155111": route_17,
+  "8453-1": route_18,
+  "8453-42161": route_19,
+  "84532-11155111": route_20,
+  "84532-421614": route_21,
 };
